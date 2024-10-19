3a33665323caa55e739b8c29a707ee95d683e963
champgm <2091382+champgm@users.noreply.github.com>
2024-10-19 14:47:55 -0400

    Add parameters to deploy file, auto-generate ovpn files

--- deploy.py
+++ deploy.py
@@ -23,6 +23,20 @@ def get_console_url(region_name, stack_id):
     return f"https://console.aws.amazon.com/cloudformation/home?region={region_name}#/stacks/stackinfo?stackId={stack_id}"
 
 
+def print_stack_outputs(cf, stack_name):
+    try:
+        response = cf.describe_stacks(StackName=stack_name)
+        stack = response["Stacks"][0]
+        outputs = stack.get("Outputs", [])
+        for output in outputs:
+            if output["OutputKey"] == "ConnectionInstructions":
+                print("\nConnection Instructions:")
+                print(output["OutputValue"])
+                break
+    except ClientError as e:
+        print(f"Failed to retrieve stack outputs for '{stack_name}'. Error: {e}")
+
+
 def track_stack_progress(cf, region_name, stack_name):
     stack_url = get_console_url(region_name, stack_name)
     print(f"Tracking stack progress, URL to CloudFormation console: {stack_url}")
@@ -39,9 +53,14 @@ def track_stack_progress(cf, region_name, stack_name):
             break
         time.sleep(10)
     print(f"Done, CloudFormation URL is: {stack_url}")
+    # If stack creation/update is complete, retrieve and print the ConnectionInstructions
+    if status.endswith("COMPLETE"):
+        print_stack_outputs(cf, stack_name)
 
 
-def deploy_cloudformation_stack(stack_name: str, region: str):
+def deploy_cloudformation_stack(
+    stack_name: str, key_pair_name: str, instance_type: str, region: str
+):
     # Create a CloudFormation client
     boto3.setup_default_session(region_name=region)
     cf = boto3.client("cloudformation")
@@ -57,7 +76,10 @@ def deploy_cloudformation_stack(stack_name: str, region: str):
         cf.create_stack(
             StackName=stack_name,
             TemplateBody=template_body,
-            Parameters=[],
+            Parameters=[
+                {"ParameterKey": "KeyPairName", "ParameterValue": key_pair_name},
+                {"ParameterKey": "InstanceType", "ParameterValue": instance_type}
+            ],
             Capabilities=["CAPABILITY_IAM"],
         )
         print(f"Stack '{stack_name}' creation initiated.")
@@ -75,13 +97,25 @@ def deploy_cloudformation_stack(stack_name: str, region: str):
     default="vpn-server",
 )
 @click.option(
+    "--key-pair-name",
+    required=True,
+    help="Name for ssh key pair",
+    default="vpn",
+)
+@click.option(
     "--region",
     required=True,
     help="The region in which the bucket should be deployed",
     default="us-west-2",
 )
-def main(stack_name: str, region: str):
-    deploy_cloudformation_stack(stack_name, region)
+@click.option(
+    "--instance-type",
+    required=True,
+    help="Instance type for the EC2 instance, see https://aws.amazon.com/ec2/instance-types/",
+    default="t3a.micro",
+)
+def main(stack_name: str, key_pair_name: str, instance_type: str, region: str):
+    deploy_cloudformation_stack(stack_name, key_pair_name, instance_type, region)
 
 
 if __name__ == "__main__":
